62b2c215b5237022ec3bd652459e631e18ef9199
Dawid Popiel <minecraftdewidos@gmail.com>
2022-08-11 10:10:30 +0200

    First commit

---
+++ src/main/java/io/github/dewidos/Atomized.java
@@ -0,0 +1,40 @@
+package io.github.dewidos;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Atomized.MOD_ID)
+public class Atomized {
+
+    public static final String MOD_ID = "atomized";
+
+//    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Atomized() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+//        Empty
+    }
+
+//    private void enqueueIMC(final InterModEnqueueEvent event) {
+//        // Some example code to dispatch IMC to another mod
+//        InterModComms.sendTo("Atomized", "helloworld", () -> {
+//            LOGGER.info("Hello world from the MDK");
+//            return "Hello world";
+//        });
+//    }
+
+//    private void processIMC(final InterModProcessEvent event) {
+//        // Some example code to receive and process InterModComms from other mods
+//        LOGGER.info("Got IMC {}", event.getIMCStream().
+//                map(m -> m.messageSupplier().get()).
+//                collect(Collectors.toList()));
+//    }
+
+}
