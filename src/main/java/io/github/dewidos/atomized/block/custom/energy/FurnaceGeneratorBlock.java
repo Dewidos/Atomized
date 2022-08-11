1164ffcce33c9551fab0b1c704aafcbccadb8def
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 15:54:32 +0200

    Adding rotation to furnace generator

--- src/main/java/io/github/dewidos/atomized/block/custom/energy/FurnaceGeneratorBlock.java
+++ src/main/java/io/github/dewidos/atomized/block/custom/energy/FurnaceGeneratorBlock.java
@@ -3,16 +3,23 @@ package io.github.dewidos.atomized.block.custom.energy;
 import io.github.dewidos.atomized.block.entity.FurnaceGeneratorBlockEntity;
 import io.github.dewidos.atomized.block.entity.ModBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class FurnaceGeneratorBlock extends Block implements EntityBlock {
+
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public FurnaceGeneratorBlock(Properties pProperties) {
         super(pProperties);
     }
@@ -27,5 +34,17 @@ public class FurnaceGeneratorBlock extends Block implements EntityBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
         return pLevel.isClientSide() ? null : ($0, $1, $2, blockEntity) -> ((FurnaceGeneratorBlockEntity)blockEntity).tick();
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        Direction direction = pContext.getHorizontalDirection().getOpposite();
+        return this.defaultBlockState().setValue(FACING, direction);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
     }
 }
